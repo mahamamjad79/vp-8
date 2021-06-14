0cf7473c9124bf4bd90fddb053266c98507a9a6c
mahamamjad79 <lunarlemonari@gmail.com>
2021-06-14 08:56:28 -0700

    Initial commit

---
+++ vp-8.js
@@ -0,0 +1,10 @@
+var type = new Typed(".typing-text",{
+    strings : ["web designer" , "front end developer", "graphic designer"],
+    typeSpeed: 120,
+    loop:true,
+});
+let themeColor = document.querySelectorAll(".theme-toggler span");
+themeColor.forEach(color =>color.addEventListener("click", ()=>{
+    let background= color.style.background;
+    document.querySelector("body").style.background= background;
+}));
